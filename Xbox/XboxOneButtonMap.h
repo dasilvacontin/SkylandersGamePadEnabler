70f7ba7e03aba37139f94d2e030d153b1368679d
Eric Betts <bettse@gmail.com>
2014-12-11 19:14:59 -0800

    whitespace

--- Xbox/XboxOneButtonMap.h
+++ Xbox/XboxOneButtonMap.h
@@ -15,30 +15,30 @@ typedef struct {
   bool dummy; // Always 0.
   bool menu;  // Not entirely sure what these are
   bool view;  // called on the new controller
-  
+
   bool a;
   bool b;
   bool x;
   bool y;
-  
+
   bool dpad_up;
   bool dpad_down;
   bool dpad_left;
   bool dpad_right;
-  
+
   bool bumper_left;
   bool bumper_right;
   bool stick_left_click;
   bool stick_right_click;
-  
+
   unsigned short trigger_left;
   unsigned short trigger_right;
-  
+
   short stick_left_x;
   short stick_left_y;
   short stick_right_x;
   short stick_right_y;
-  
+
   bool home;
 } XboxOneButtonMap;
 
